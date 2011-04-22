82f3c27c3d1f1268cec7ca62696108dc4203f92c
Dan Frank <danielhfrank@gmail.com>
2011-04-21 23:37:51 -0400

    fixed!

--- playlist_repairer.py
+++ playlist_repairer.py
@@ -6,18 +6,7 @@ relative pathnames so that it can all be sent to someone else
 
 import os
 import sys
-
-def escape(x):
-	tmp = x.replace("\n", "")
-	tmp = tmp.replace("\r", "")
-	tmp = tmp.replace(" ", "\ ")
-	tmp = tmp.replace("&", "\&")
-	tmp = tmp.replace(",", "\,")
-	tmp = tmp.replace("\'", "\\\'")#had to escape this one differently (fewer \) on ubuntu
-	tmp = tmp.replace("\"", "\\\"")
-	tmp = tmp.replace("(", "\(")
-	tmp = tmp.replace(")", "\)")
-	return tmp
+import shutil
 
 m3us = filter(lambda x: x.endswith(".m3u"),os.listdir("."))
 if len(m3us) == 0:
@@ -30,27 +19,30 @@ f = open(m3u, 'r')
 contents = f.readline()
 f.close()
 
-#print contents
 lines = contents.split("\r")#had to escape this one differently (more \) on ubuntu
-for line in lines:
-#	print ">>>" + line
 absPaths = [x for x in lines if x.startswith("/")]
-escapedPaths = [escape(x) for x in absPaths]
 
 #Make a dir for all this good stuff
-dr = escape(m3u[:-4])
-os.system("mkdir " + dr)
+#dr = escape(m3u[:-4])
+#os.system("mkdir " + dr)
+dr = m3u[:-4]
+os.mkdir(dr)
 
 #Copy all the songs into the dir - can be done in one call to cp
-call = "cp " + " ".join(escapedPaths) +  " " + dr
-print call
-os.system(call.strip())
+#call = "cp " + " ".join(escapedPaths) +  " " + dr
+#print call
+#os.system(call.strip())
+for songPath in absPaths:
+	shutil.copy(songPath, dr)
+
 
 #and now write them all to a file
 f2 = open(dr + "/" + m3u, 'w')
 f2.write("\r".join(['#EXTM3U'] + [x.split("/")[-1] for x in absPaths]))
 f2.close()
 
+print 'FUKYEA'
+
 #finally, trash that original m3u file
-os.system("rm -f " + m3u)
+#os.system("rm -f " + m3u)
 	
